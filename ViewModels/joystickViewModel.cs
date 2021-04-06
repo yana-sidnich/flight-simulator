0e6ab8725497456045c383a5dce7cc4b4be167d3
Itay <iyacov@gmail.com>
2021-04-07 00:58:07 +0300

    Include graphs template (without actual graphs)

--- ViewModels/joystickViewModel.cs
+++ ViewModels/joystickViewModel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 
 
-namespace FlightGearTestExec
+namespace FlightGearTestExec.ViewModels
 {
     class joystickViewModel : INotifyPropertyChanged
     {
---
+++ Views/FeaturesListView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using static FlightGearTestExec.FlightDataContainer;
+using FlightGearTestExec.ViewModels;
+
+namespace FlightGearTestExec.Views
+{
+    public partial class FeaturesListView : UserControl, INotifyPropertyChanged
+    {
+        private struct ListItem
+        {
+            public string FeatureName { get; set; }
+            public string CorrelatedFeatures { get; set; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Selected
+        {
+            get { return _selected; }
+            set
+            {
+                _selected = value;
+                NotifyPropertyChanged("Selected");
+            }
+        }
+
+        private int _selected;
+        // <src:UserControl DataContext="{Binding Path=DataContext.UserViewModel, RelativeSource={RelativeSource={RelativeSource Mode=FindAncestor, AncestorType={x:Type Window}}}">
+        // <someNamespace:EricsUserControl DataContext="{Binding InstanceOfBindingObject}"/>
+        public FeaturesListView()
+        {
+            InitializeComponent();
+            // vm = new FeaturesListViewModel();
+            Dictionary<string, FlightDataContainer> dataDictionary = get_should_be_data_context();
+            foreach (var pair in dataDictionary)
+            {
+                ListItem temp = new ListItem { FeatureName = pair.Key, CorrelatedFeatures = pair.Value.correlatedFeatureName };
+                FeaturesList.Items.Add(temp);
+            }
+        }
+
+        private void List_Selected(object sender, RoutedEventArgs e)
+        {
+            ListView list = sender as ListView;
+            Selected = list.SelectedIndex;
+        }
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
