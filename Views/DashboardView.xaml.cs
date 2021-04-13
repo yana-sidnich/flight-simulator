fd3c257b1c98a6982f8a3d61b226219229ce4ad7
YairLevi <79797419+YairLevi@users.noreply.github.com>
2021-04-14 00:12:20 +0300

    New Dashboard View
    adding the new dashboard view files.

---
+++ Views/DashboardView.xaml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FlightGearTestExec.ViewModels;
+using static FlightGearTestExec.Models.FlightDataContainer;
+
+namespace FlightGearTestExec.Views
+{
+    public partial class DashboardView : UserControl
+    {
+        private DashboardViewModel vm;
+
+        public DashboardView()
+        {
+            vm = new DashboardViewModel();
+            InitializeComponent();
+            this.DataContext = vm;
+        }
+    }
+}
+
